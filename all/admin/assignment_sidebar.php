61daa20c810d0250f22fb2e9746ee4819452581e
Bence Papp <113164265+pbence11@users.noreply.github.com>
2024-11-24 10:55:34 +0100

    Calendar function and sidebars

---
+++ all/admin/assignment_sidebar.php
@@ -0,0 +1,44 @@
+		 <div class="span3" id="sidebar">
+						<ul class="nav nav-list bs-docs-sidenav nav-collapse collapse">
+							<li> <a href="dashboard.php"><i class="icon-chevron-right"></i><i class="icon-home"></i>&nbsp;Felületkezelő</a> </li>
+							<li>
+								<a href="subjects.php"><i class="icon-chevron-right"></i><i class="icon-list-alt"></i> Tantárgy</a>
+							</li>
+							<li>
+								<a href="class.php"><i class="icon-chevron-right"></i><i class="icon-group"></i> Csoport</a>
+							</li>
+							<li>
+								<a href="admin_user.php"><i class="icon-chevron-right"></i><i class="icon-user"></i> Rendszergazdai felhasználók</a>
+							</li>
+							<li>
+								<a href="department.php"><i class="icon-chevron-right"></i><i class="icon-building"></i> Tanterület</a>
+							</li>
+							<li>
+								<a href="students.php"><i class="icon-chevron-right"></i><i class="icon-group"></i> Hallgatók</a>
+							</li>
+							<li>
+								<a href="teachers.php"><i class="icon-chevron-right"></i><i class="icon-group"></i> Oktatók</a>
+							</li>
+							<li>
+								<a href="downloadable.php"><i class="icon-chevron-right"></i><i class="icon-download"></i> Letölthető elemek</a>
+							</li>
+							<li  class="active">
+								<a href="assignment.php"><i class="icon-chevron-right"></i><i class="icon-upload"></i> Feltöltető elemek</a>
+							</li>
+							<li>
+								<a href="content.php"><i class="icon-chevron-right"></i><i class="icon-file"></i> Tartalom</a>
+							</li>
+							<li>
+								<a href="user_log.php"><i class="icon-chevron-right"></i><i class="icon-file"></i> Felhasználói napló</a>
+							</li>
+							<li>
+								<a href="activity_log.php"><i class="icon-chevron-right"></i><i class="icon-file"></i> Akitivitási napló</a>
+							</li>
+							<li>
+                            <a href="school_year.php"><i class="icon-chevron-right"></i><i class="icon-calendar"></i> Tanév</a>
+							</li>
+							<li class="">
+                            <a href="calendar_of_events.php"><i class="icon-chevron-right"></i><i class="icon-calendar"></i>Eseménynaptár</a>
+							</li>
+						</ul>
+					</div>
---
+++ all/admin/calendar.php
@@ -0,0 +1,182 @@
+<?php  include('header.php'); ?>
+    
+    <body>
+		<?php include('navbar.php'); ?>
+        <div class="container-fluid">
+            <div class="row-fluid">
+				<?php include('sidebar.php'); ?>
+                <!--/span-->
+                <div class="span9" id="content">
+                    <div class="row-fluid">
+                        <!-- block -->
+                        <div class="block">
+                            <div class="navbar navbar-inner block-header">
+                                <div class="muted pull-left">Naptár</div>
+                                <div class="pull-right"><span class="badge badge-warning">Láss többet...</span>
+
+                                </div>
+                            </div>
+                            <div class="block-content collapse in">
+                                <div class="span2">
+                                    <div id='external-events'>
+                                    <h4>Elhúzható események</h4>
+                                    <div class='external-event'>1. esemény</div>
+                                    <div class='external-event'>2. esemény</div>
+                                    <div class='external-event'>3. esemény</div>
+                                    <div class='external-event'>4. esemény</div>
+                                    <div class='external-event'>5. esemény</div>
+                                    <div class='external-event'>6. esemény</div>
+                                    <div class='external-event'>7. esemény</div>
+                                    <div class='external-event'>8. esemény</div>
+                                    <div class='external-event'>9. esemény</div>
+                                    <div class='external-event'>10. esemény</div>
+                                    <div class='external-event'>11. esemény</div>
+                                    <div class='external-event'>12. esemény</div>
+                                    <div class='external-event'>13. esemény</div>
+                                    <div class='external-event'>14. esemény</div>
+                                    <div class='external-event'>15. esemény</div>
+                                    <p>
+                                    <input type='checkbox' id='drop-remove' /> <label for='drop-remove'>Törlés esemény eldobása után</label>
+                                    </p>
+                                    </div>
+
+                                </div>
+                                <div class="span10">
+                                    <div id='calendar'></div>
+                                </div>
+                            </div>
+                        </div>
+                        <!-- /block -->
+                    </div>
+                </div>
+            </div>
+            <hr>
+            <footer>
+                <p>&copy; Vincent Gabriel - 2023</p>
+            </footer>
+        </div>
+        <style>
+        #external-events {
+            float: left;
+            width: 150px;
+            padding: 0 10px;
+            border: 1px solid #ccc;
+            background: #eee;
+            text-align: left;
+            }
+            
+        #external-events h4 {
+            font-size: 16px;
+            margin-top: 0;
+            padding-top: 1em;
+            }
+            
+        .external-event { /* try to mimick the look of a real event */
+            margin: 10px 0;
+            padding: 2px 4px;
+            background: #3366CC;
+            color: #fff;
+            font-size: .85em;
+            cursor: pointer;
+            z-index: 99999999;
+            }
+            
+        #external-events p {
+            margin: 1.5em 0;
+            font-size: 11px;
+            color: #666;
+            }
+            
+        #external-events p input {
+            margin: 0;
+            vertical-align: middle;
+            }
+
+        </style>
+        <!--/.fluid-container-->
+        <script src="vendors/jquery-1.9.1.min.js"></script>
+        <script src="vendors/jquery-ui-1.10.3.js"></script>
+        <script src="bootstrap/js/bootstrap.min.js"></script>
+        <script src="vendors/fullcalendar/fullcalendar.js"></script>
+        <script src="vendors/fullcalendar/gcal.js"></script>
+        <script src="assets/scripts.js"></script>
+        <script>
+        $(function() {
+            // Easy pie charts
+            var calendar = $('#calendar').fullCalendar({
+			header: {
+				left: 'prev,next',
+				center: 'title',
+				right: 'month,basicWeek,basicDay'
+			},
+            selectable: true,
+            selectHelper: true,
+            select: function(start, end, allDay) {
+                var title = prompt('Esemény neve:');
+                if (title) {
+                    calendar.fullCalendar('renderEvent',
+                        {
+                            title: title,
+                            start: start,
+                            end: end,
+                            allDay: allDay
+                        },
+                        true // make the event "stick"
+                    );
+                }
+                calendar.fullCalendar('unselect');
+            },
+            droppable: true, // this allows things to be dropped onto the calendar !!!
+            drop: function(date, allDay) { // this function is called when something is dropped
+            
+                // retrieve the dropped element's stored Event Object
+                var originalEventObject = $(this).data('eventObject');
+                
+                // we need to copy it, so that multiple events don't have a reference to the same object
+                var copiedEventObject = $.extend({}, originalEventObject);
+                
+                // assign it the date that was reported
+                copiedEventObject.start = date;
+                copiedEventObject.allDay = allDay;
+                
+                // render the event on the calendar
+                // the last `true` argument determines if the event "sticks" (http://arshaw.com/fullcalendar/docs/event_rendering/renderEvent/)
+                $('#calendar').fullCalendar('renderEvent', copiedEventObject, true);
+                
+                // is the "remove after drop" checkbox checked?
+                if ($('#drop-remove').is(':checked')) {
+                    // if so, remove the element from the "Draggable Events" list
+                    $(this).remove();
+                }
+                
+            },
+			editable: true,
+			// US Holidays
+			events: 'http://www.google.com/calendar/feeds/usa__en%40holiday.calendar.google.com/public/basic'
+			
+			});
+        });
+
+        $('#external-events div.external-event').each(function() {
+        
+            // create an Event Object (http://arshaw.com/fullcalendar/docs/event_data/Event_Object/)
+            // it doesn't need to have a start or end
+            var eventObject = {
+                title: $.trim($(this).text()) // use the element's text as the event title
+            };
+            
+            // store the Event Object in the DOM element so we can get to it later
+            $(this).data('eventObject', eventObject);
+            
+            // make the event draggable using jQuery UI
+            $(this).draggable({
+                zIndex: 999999999,
+                revert: true,      // will cause the event to go back to its
+                revertDuration: 0  //  original position after the drag
+            });
+            
+        });
+        </script>
+    </body>
+
+</html>
