d22b54f689733f44f8c1a1a9e6c429d2b2cac74f
vkatasz <varga.kata@atakv.hu>
2024-11-25 10:25:33 +0100

    Added modal_delete.php

---
+++ all/admin/modal_delete.php
@@ -0,0 +1,120 @@
+					<!-- user delete model -->
+					<div id="user_delete" class="modal hide fade" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
+					<div class="modal-header">
+					<button type="button" class="close" data-dismiss="modal" aria-hidden="true">X</button>
+					<h3 id="myModalLabel">Törli a felhasználókat?</h3>
+					</div>
+					<div class="modal-body">
+					<div class="alert alert-danger">
+					<p>Biztos, hogy törölni akarja az ellenőrzött felhasználót?</p>
+					</div>
+					</div>
+					<div class="modal-footer">
+					<button class="btn" data-dismiss="modal" aria-hidden="true"><i class="icon-remove icon-large"></i> Bezárás</button>
+					<button name="delete_user" class="btn btn-danger"><i class="icon-check icon-large"></i> Törlés</button>
+					</div>
+					</div>
+					
+					<!-- department delete model -->
+					<div id="department_delete" class="modal hide fade" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
+					<div class="modal-header">
+					<button type="button" class="close" data-dismiss="modal" aria-hidden="true">x</button>
+					<h3 id="myModalLabel">Törli az adott tanterületet?</h3>
+					</div>
+					<div class="modal-body">
+					<div class="alert alert-danger">
+					<p>Biztos, hogy törölni akarja a bejelölt tanterületet?</p>
+					</div>
+					</div>
+					<div class="modal-footer">
+					<button class="btn" data-dismiss="modal" aria-hidden="true"><i class="icon-remove icon-large"></i> Bezárás</button>
+					<button name="delete_department" class="btn btn-danger"><i class="icon-check icon-large"></i> Törlés</button>
+					</div>
+					</div>
+					
+								<!-- class delete model -->
+					<div id="class_delete" class="modal hide fade" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
+					<div class="modal-header">
+					<button type="button" class="close" data-dismiss="modal" aria-hidden="true">x</button>
+					<h3 id="myModalLabel">Törli az adott csoportot?</h3>
+					</div>
+					<div class="modal-body">
+					<div class="alert alert-danger">
+					<p>Biztos, hogy törölni akarja az ellenőrzött osztályt?</p>
+					</div>
+					</div>
+					<div class="modal-footer">
+					<button class="btn" data-dismiss="modal" aria-hidden="true"><i class="icon-remove icon-large"></i> Bezárás</button>
+					<button name="delete_class" class="btn btn-danger"><i class="icon-check icon-large"></i> Törlés</button>
+					</div>
+					</div>
+					
+											<!-- student delete model -->
+					<div id="student_delete" class="modal hide fade" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
+					<div class="modal-header">
+					<button type="button" class="close" data-dismiss="modal" aria-hidden="true">x</button>
+					<h3 id="myModalLabel">Törli az adott hallgatót?</h3>
+					</div>
+					<div class="modal-body">
+					<div class="alert alert-danger">
+					<p>Biztos, hogy törölni szeretné az ellenőrzött tanulót?</p>
+					</div>
+					</div>
+					<div class="modal-footer">
+					<button class="btn" data-dismiss="modal" aria-hidden="true"><i class="icon-remove icon-large"></i> Bezárás</button>
+					<button name="delete_student" class="btn btn-danger"><i class="icon-check icon-large"></i> Törlés</button>
+					</div>
+					</div>
+					
+					
+											<!-- student delete model -->
+					<div id="teacher_delete" class="modal hide fade" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
+					<div class="modal-header">
+					<button type="button" class="close" data-dismiss="modal" aria-hidden="true">x</button>
+					<h3 id="myModalLabel">Törli az adott oktatót?</h3>
+					</div>
+					<div class="modal-body">
+					<div class="alert alert-danger">
+					<p>Biztos, hogy törölni akarja az ellenőrzött tanárt?</p>
+					</div>
+					</div>
+					<div class="modal-footer">
+					<button class="btn" data-dismiss="modal" aria-hidden="true"><i class="icon-remove icon-large"></i> Bezárás</button>
+					<button name="delete_teacher" class="btn btn-danger"><i class="icon-check icon-large"></i> Törlés</button>
+					</div>
+					</div>
+					
+					
+					                 	<!-- Content delete model -->
+					<div id="content_delete" class="modal hide fade" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
+					<div class="modal-header">
+					<button type="button" class="close" data-dismiss="modal" aria-hidden="true">x</button>
+					<h3 id="myModalLabel">Törli az adott tartalmat?</h3>
+					</div>
+					<div class="modal-body">
+					<div class="alert alert-danger">
+					<p>Biztos, hogy törölni szeretné az ellenőrzött tartalmat?</p>
+					</div>
+					</div>
+					<div class="modal-footer">
+					<button class="btn" data-dismiss="modal" aria-hidden="true"><i class="icon-remove icon-large"></i> Bezárás</button>
+					<button name="delete_content" class="btn btn-danger"><i class="icon-check icon-large"></i> Törlés</button>
+					</div>
+					</div>
+
+					<!-- Content delete model -->
+					<div id="subject_delete" class="modal hide fade" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
+					<div class="modal-header">
+					<button type="button" class="close" data-dismiss="modal" aria-hidden="true">x</button>
+					<h3 id="myModalLabel">Törli az adott tantárgyat?</h3>
+					</div>
+					<div class="modal-body">
+					<div class="alert alert-danger">
+					<p>Biztos, hogy törölni akarja az ellenőrzött témát?</p>
+					</div>
+					</div>
+					<div class="modal-footer">
+					<button class="btn" data-dismiss="modal" aria-hidden="true"><i class="icon-remove icon-large"></i> Bezárás</button>
+					<button name="delete_subject" class="btn btn-danger"><i class="icon-check icon-large"></i> Törlés</button>
+					</div>
+					</div>
