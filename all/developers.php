e67030d469ac0c39b8b59c809b4134f3b60a42ed
vkatasz <varga.kata@atakv.hu>
2024-12-08 00:20:44 +0100

    developers.php was added

---
+++ all/developers.php
@@ -0,0 +1,58 @@
+<?php include('header_dashboard.php'); ?>
+    <body id="class_div">
+		<?php include('navbar_about.php'); ?>
+        <div class="container-fluid">
+            <div class="row-fluid">
+                <div class="span12" id="content">
+                     <div class="row-fluid">
+                        <!-- block -->
+                        <div class="block">
+								<div class="navbar navbar-inner block-header">
+									<div id="" class="muted pull-right"><a href="index.php"><i class="icon-arrow-left"></i> Visszalépés</a></div>
+								</div>
+                            <div class="block-content collapse in">
+							<h3>Developers</h3>
+							<hr>
+								<div class="span3">
+								<center>
+								<img id="developers" src="admin/images/vargaksz.jpg" class="img-circle">
+								<hr>
+										<p>Név: Varga Kata Szilvia</p>
+										<p>Beosztás: Fejlesztő</p>
+								</center>
+								</div>
+                                <div class="span3">
+								<center>
+								<img id="developers" src="admin/images/szabob.jpg" class="img-circle">
+								<hr>
+										<p>Név: Szabó Benedek</p>
+										<p>Beosztás: Fejlesztő</p>
+								</center>
+								</div>
+                                <div class="span3">
+								<center>
+								<img id="developers" src="admin/images/pappb.jpg" class="img-circle">
+								<hr>
+										<p>Name: Papp Bence</p>
+										<p>Beosztás: Fejlesztő</p>
+								</center>
+								</div>
+                                <div class="span3">
+								<center>
+								<img id="developers" src="admin/images/barathym.jpg" class="img-circle">
+								<hr>
+										<p>Név: Barathy Marcell</p>
+										<p>Beosztás: Fejlesztő</p>
+								</center>
+								</div>
+                            </div>
+                        </div>
+                        <!-- /block -->
+                    </div>
+                </div>
+            </div>
+		<?php include('footer.php'); ?>
+        </div>
+		<?php include('script.php'); ?>
+    </body>
+</html>
