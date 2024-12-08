81d8197486123a52c016ccd5708fde9ae6f18090
Szabó Benedek <szabo.benedek012@gmail.com>
2024-12-08 13:13:46 +0100

    Added link phps

---
+++ all/link.php
@@ -0,0 +1,29 @@
+<div class="navbar">
+           <div class="navbar-inner">
+               <div class="container-fluid">
+					<a class="btn btn-navbar" data-toggle="collapse" data-target=".nav-collapse"> <span class="icon-bar"></span>
+						<span class="icon-bar"></span>
+						<span class="icon-bar"></span>
+					</a>
+							<div class="nav-collapse collapse">
+							<ul class="nav" id="footer_nav">
+			<li class="divider-vertical"></li>
+		<li class="active"><a href="index.php"><i class="icon-home"></i>&nbsp;Főoldal</a></li>
+			<li class="divider-vertical"></li>
+		<li><a href="about.php"><i class="icon-info-sign"></i>&nbsp;Rólunk</a></li>
+			<li class="divider-vertical"></li>
+		<li><a href="calendar_of_events.php"><i class="icon-calendar"></i>&nbsp;Eseménynaptár</a></li>
+			<li class="divider-vertical"></li>
+		<li><a href="#directories" data-toggle="modal"><i class="icon-phone"></i>&nbsp;Elérhetőség</a></li>
+				<?php include('directories.php'); ?>
+			<li class="divider-vertical"></li>
+		<li><a href="history.php"><i class="icon-file"></i>&nbsp;Történetünk</a></li>
+			<li class="divider-vertical"></li>
+		<li><a href="developers.php"><i class="icon-user"></i>&nbsp;Fejlesztőink</a></li>
+		<li class="divider-vertical"></li>
+	</ul>
+							</div>
+                   <!--/.nav-collapse -->
+               </div>
+           </div>
+</div>
