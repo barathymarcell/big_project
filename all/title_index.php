835c660cd01dfdbd147e66f74afedda720c2399b
Bence Papp <113164265+pbence11@users.noreply.github.com>
2024-12-08 14:03:46 +0100

    Basic function updated

---
+++ all/title_index.php
@@ -0,0 +1,39 @@
+				
+								<div class="row-fluid">
+
+						<div class="span12">
+						
+						</div>	
+													
+							</div>
+							<div class="row-fluid">
+
+						<div class="span4">
+						<img class="index_logo" src="admin/images/gb_logo.png">
+						<link rel="title_icon" href="admin/images/gb_logo.png" type="x-icon">
+						</div>	
+						<div class="span8">
+						
+								<div class="title">
+							<p class="chmsc">GitBarmok ELearning Kezelői Felülete @ 2023</p>
+							<h3>
+
+							<p>GitBarmok</p>
+						
+							</h3>		
+						</div>
+			
+						</div>							
+							</div>
+				
+				<div class="row-fluid">
+
+						<div class="span12">
+						<br>
+								<div class="motto">
+												<p>Miről vagyunk ismertek?</p>
+												<p>Kiválóság, Kompetencia és Oktatás</p>
+												<p>Vezető szerep a tudomány és a technológia területén</p>
+								</div>		
+						</div>		
+				</div>
